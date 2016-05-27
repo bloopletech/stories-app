198d7238e545c2e829ade232301e469d979dbadd
Brenton Fletcher <i@bloople.net>
2016-05-28 01:20:47 +1000

    Add 3rd-party file picker; Remove action bar

--- app/src/main/java/bloople/net/stories/MainActivity.java
+++ app/src/main/java/bloople/net/stories/MainActivity.java
@@ -1,13 +1,20 @@
 package bloople.net.stories;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.io.File;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView nodesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
         Utils.verifyStoragePermissions(this);
 
-        RecyclerView nodesView = (RecyclerView)findViewById(R.id.nodes_view);
+        nodesView = (RecyclerView)findViewById(R.id.nodes_view);
 
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         nodesView.setLayoutManager(linearLayoutManager);
 
-        final List<CharSequence> nodes = (new StoryOpener(this)).open().nodes();
-        final NodesAdapter adapter = new NodesAdapter(nodes);
-        nodesView.setAdapter(adapter);
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(1)
+                .withFilter(Pattern.compile(".*\\.html?")) // Filtering files and directories by file name using regexp
+                .start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+            final List<CharSequence> nodes = (new StoryOpener()).open(new File(path)).nodes();
+            final NodesAdapter adapter = new NodesAdapter(nodes);
+            nodesView.setAdapter(adapter);
+        }
     }
 }
--- app/src/main/java/bloople/net/stories/StoryOpener.java
+++ app/src/main/java/bloople/net/stories/StoryOpener.java
@@ -1,8 +1,5 @@
 package bloople.net.stories;
 
-import android.app.Activity;
-import android.os.Environment;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -10,17 +7,8 @@ import java.io.IOException;
  * Created by i on 27/05/2016.
  */
 public class StoryOpener {
-    private Activity activity;
-
-    public StoryOpener(Activity inActivity) {
-        activity = inActivity;
-    }
-
-    public Story open() {
+    public Story open(File file) {
         try {
-            File file = new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS), "test2.html");
-
             StoryParser parser = new StoryParser(file);
             return parser.parse();
         }
