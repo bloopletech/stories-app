fd313a1f7e0a8f168cb55f5f8e13885d9ed0a32e
Brenton Fletcher <i@bloople.net>
2016-07-07 19:24:10 +1000

    Ask for storage permissions correctly and wait for result before launching picker; Remove unused Utils class

--- app/src/main/java/net/bloople/stories/MainActivity.java
+++ app/src/main/java/net/bloople/stories/MainActivity.java
@@ -1,7 +1,11 @@
 package net.bloople.stories;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
@@ -9,26 +13,41 @@ import com.nbsp.materialfilepicker.MaterialFilePicker;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Utils.verifyStoragePermissions(this);
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        MaterialFilePicker picker = new MaterialFilePicker();
-        picker.withActivity(this).withClass(ReadingStoryActivity.class).withFilter(
-                Pattern.compile(".*\\.txt?"));
-
-        picker.start();
+        if(permission == PackageManager.PERMISSION_GRANTED) launchPicker();
+        else ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_CANCELED) {
-            finish();
-        }
+        if(resultCode == RESULT_CANCELED) finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(requestCode == REQUEST_EXTERNAL_STORAGE && grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) launchPicker();
+    }
+
+    private void launchPicker() {
+        MaterialFilePicker picker = new MaterialFilePicker();
+        picker.withActivity(this).withClass(ReadingStoryActivity.class).withFilter(Pattern.compile(".*\\.txt?"));
+
+        picker.start();
     }
 }
