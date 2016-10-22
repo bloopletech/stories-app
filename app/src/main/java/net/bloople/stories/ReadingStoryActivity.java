d0664d2b08a8b32d12a613b09c6d561abe480242
Brenton Fletcher <i@bloople.net>
2016-10-22 20:58:11 +1100

    Undo change that restored to first visible rather than first completely visible paragraph on story, as restoring to first completely visible paragraph will skip part of the previous paragraph

--- app/src/main/java/net/bloople/stories/ReadingStoryActivity.java
+++ app/src/main/java/net/bloople/stories/ReadingStoryActivity.java
@@ -43,7 +43,7 @@ public class ReadingStoryActivity extends Activity {
     protected void onStop() {
         super.onStop();
 
-        book.lastReadPosition(layoutManager.findFirstCompletelyVisibleItemPosition());
+        book.lastReadPosition(layoutManager.findFirstVisibleItemPosition());
         book.save(this);
     }
 
